f30505bbf6d293c531019ed95efd85dd5a8c71a3
thanhdo3044 <phamthanhdo18@gmail.com>
2024-03-15 17:52:50 +0700

    first commit

---
+++ resources/views/template/nguoi_dung.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('nguoi_dung/css/style.css') }}">
+    <script src="{{ asset('nguoi_dung/js/index.js') }}"></script>
+</head>
+<body>
+    Pham Thanh Do
+</body>
+</html>
