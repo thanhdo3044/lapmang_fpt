d1c79c826a2ddd021f77eef32713979c32c4dd25
0365029527Viet <vietpham08062003@gmail.com>
2024-03-16 15:09:19 +0700

    xong crud blog

---
+++ app/Models/baiviet.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class baiviet extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id_dia_chi',
+        'title',
+        'slug',
+        'description',
+    ];
+    public function tinhthanh() {
+        return $this->belongsTo(tinhthanh::class, 'id_dia_chi','id');
+    }
+    public function khuvuc() {
+        return $this->belongsTo(khuvuc::class, 'id_dia_chi', 'id');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\admin\BlogTinhThanh;
+use App\Http\Controllers\admin\khuvucController;
+use App\Http\Controllers\admin\tinhthanhController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('template.nguoi_dung');
+Route::get('/dashboard', function () {
+    return view('template.admin');
 });
+Route::get('/khu-vuc', [khuvucController::class, 'index'])->name('khuvuc.index');
+Route::get('/add-khu-vuc', [khuvucController::class, 'create'])->name('khuvuc.create');
+Route::post('/add-khu-vuc', [khuvucController::class, 'store'])->name('khuvuc.store');
+Route::get('/xoa-khu-vuc/{id}', [khuvucController::class, 'destroy'])->name('khuvuc.delete');
+Route::get('/sua-khu-vuc/{id}', [khuvucController::class, 'show'])->name('khuvuc.edit');
+Route::post('/sua-khu-vuc/{id}', [khuvucController::class, 'update'])->name('khuvuc.update');
+
+
+Route::get('/thanh-pho', [tinhthanhController::class, 'index'])->name('tinhthanh.index');
+Route::get('/add-thanh-pho', [tinhthanhController::class, 'create'])->name('tinhthanh.create');
+Route::post('/add-thanh-pho', [tinhthanhController::class, 'store'])->name('tinhthanh.store');
+Route::get('/xoa-thanh-pho/{id}', [tinhthanhController::class, 'destroy'])->name('tinhthanh.delete');
+Route::get('/sua-thanh-pho/{id}', [tinhthanhController::class, 'show'])->name('tinhthanh.edit');
+Route::post('/sua-thanh-pho/{id}', [tinhthanhController::class, 'update'])->name('tinhthanh.update');
+
+
+Route::get('/bai-viet-tinh-thanh', [BlogTinhThanh::class, 'index'])->name('blog.index');
+Route::get('/add-bai-viet-tinh-thanh', [BlogTinhThanh::class, 'create'])->name('blog.create');
+Route::match(['get', 'post'], '/upload', [BlogTinhThanh::class, 'upload'])->name('ckedit.upload');
+Route::post('/add-bai-viet-tinh-thanh', [BlogTinhThanh::class, 'store'])->name('blog.store');
+Route::get('/sua-bai-viet/{id}', [BlogTinhThanh::class, 'show'])->name('blog.edit');
+Route::post('/sua-bai-viet/{id}', [BlogTinhThanh::class, 'update'])->name('blog.update');
