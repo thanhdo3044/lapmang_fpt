dc8db848ddcbc103b1400b2488487183fcd25e90
0365029527Viet <vietpham08062003@gmail.com>
2024-04-01 17:29:36 +0700

    100%

--- resources/views/admin/baiviet/menu/edit.blade.php
+++ resources/views/admin/baiviet/menu/edit.blade.php
@@ -23,7 +23,7 @@
           </div>
           <div class="mb-3">
             <label for="" class="form-label">Tên Menu</label>
-            <input type="text" class="form-control" name="id_dia_chi" value="{{$data->id_dia_chi}}" disabled/>
+            <input type="text" class="form-control" name="id_dia_chi" value="{{$data->id_dia_chi}}"/>
 
             <div class="mb-3">
                 <label for="" class="form-label">Key word</label>
