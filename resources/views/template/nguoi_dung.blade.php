32c9d44babf3992d909eb2efa2599b8ca9ac62bd
thanhdo3044 <phamthanhdo18@gmail.com>
2024-04-02 13:21:20 +0700

    sau loi css giao dien nguoi dung

--- resources/views/template/nguoi_dung.blade.php
+++ resources/views/template/nguoi_dung.blade.php
@@ -82,7 +82,7 @@
                 </div>
 
                 <div class="col-2 d-flex justify-content-center align-items-center">
-                    <img class="title-mobi" src="{{ asset('nguoi_dung/img/box_103x38.png') }}" alt="logofptmobile" />
+                    <img class="title-mobi" src="{{ asset('nguoi_dung/img/icon-fcamera.png') }}" width="90" alt="logofptmobile" />
                 </div>
             </div>
 
@@ -585,41 +585,20 @@
             </div>
     
             <div style="color: #000080;" id="DienThoai2">
-                <div class="icon_lienhe" id="id_chat_lieuhe">
+                <div class="icon_lienhe_sdt" >
                     <a href="tel:0988638092">
-                        <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
-                            <rect fill="none" />
-                            <path d="M159.4,40A80.1,80.1,0,0,1,216,96.6" fill="none" stroke="#000"
-                                stroke-linecap="round" stroke-linejoin="round" stroke-width="16" />
-                            <path d="M151.1,70.9a47.9,47.9,0,0,1,34,34" fill="none" stroke="#000"
-                                stroke-linecap="round" stroke-linejoin="round" stroke-width="16" />
-                            <path
-                                d="M92.5,124.8a83.6,83.6,0,0,0,39,38.9,8,8,0,0,0,7.9-.6l25-16.7a7.9,7.9,0,0,1,7.6-.7l46.8,20.1a7.9,7.9,0,0,1,4.8,8.3A48,48,0,0,1,176,216,136,136,0,0,1,40,80,48,48,0,0,1,81.9,32.4a7.9,7.9,0,0,1,8.3,4.8l20.1,46.9a8,8,0,0,1-.6,7.5L93,117A8,8,0,0,0,92.5,124.8Z"
-                                fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round"
-                                stroke-width="16" />
-                        </svg>
+                        <i class="fas fa-phone-volume">   0988638092</i>
                     </a>
                 </div>
             </div>
     
-            <div style="color: #000080;" id="DienThoai3">
-    
-                <div class="icon_lienhe" id="id_chat_lieuhe">
-                    <a href="https://www.facebook.com/profile.php?id=100000930934345&mibextid=dGKdO6">
-                        <img src="{{ asset('nguoi_dung/img/2023_Facebook_icon.svg.png') }}" width="38"
-                            alt="icon face" />
-                    </a>
-                </div>
-    
-            </div>
-
         </div>
 
     
         <div class="fixe_menu_mobi">
             <div class="icon_lieuhemobi">
                 <a href="tel:0988638092">
-                    <i class="fas fa-phone-volume">0988638092</i>
+                    <i class="fas fa-phone-volume"></i>    0988638092
                 </a>
             </div>
         </div>
